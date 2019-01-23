b541d85342162af1479cd4ed2cb97a992f9dfad7
rmalyavc <white_men@bigmir.net>
2019-01-23 23:03:07 +0200

    Style fixes

--- routes/config/connection.js
+++ routes/config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Stanly106601",
+  password: "",
   database: "matcha",
   multipleStatements: true
 });
